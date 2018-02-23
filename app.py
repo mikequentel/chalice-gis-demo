04fba3bcfc6444b4150714e960029c8161b2b948
mikequentel <mikequentel@yahoo.com>
2018-02-23 13:39:14 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import json
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import sys
+from chalice import Chalice
+app = Chalice(app_name='chalice-gis-demo')
+
+@app.route('/restaurants')
+def index():
+  conn_string = "host='localhost' dbname='businesses' user='postgres' password='postgres'"
+  conn = psycopg2.connect(conn_string)
+  cursor = conn.cursor(cursor_factory=RealDictCursor)
+  cursor.execute('SELECT * FROM public.restaurants LIMIT 100')
+  records = cursor.fetchall()
+  return {'results':str(records)}
+
+@app.route('/hello/{name}')
+def hello_name(name):
+   # '/hello/james' -> {"hello": "james"}
+   return {'hello': name}
+#
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NAME=$1
+if [ -z "${NAME}" ]; then
+  echo "Include name of project."
+  exit 1
+fi
+virtualenv --python $(which python) ~/.virtualenvs/$NAME
+source ~/.virtualenvs/$NAME/bin/activate
+pip install chalice
+chalice new-project $NAME

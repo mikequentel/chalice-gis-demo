7d794f71df45145d3cff5277fb561e3596f667cc
mikequentel <mikequentel@yahoo.com>
2018-02-23 14:44:22 -0500

    get_facility

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ from psycopg2.extras import RealDictCursor
 import sys
 from chalice import Chalice
 app = Chalice(app_name='chalice-gis-demo')
+conn_string = "host='localhost' dbname='businesses' user='postgres' password='postgres'"
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor(cursor_factory=RealDictCursor)
 
 @app.route('/restaurants')
-def index():
-  conn_string = "host='localhost' dbname='businesses' user='postgres' password='postgres'"
-  conn = psycopg2.connect(conn_string)
-  cursor = conn.cursor(cursor_factory=RealDictCursor)
+def restaurants():
   cursor.execute('SELECT * FROM public.restaurants LIMIT 100')
+  records = cursor.fetchall()
+  return {'results':str(records)}
+
+# @app.route('/restaurants/facility/(?<facility>.*)')
+# @app.route('/restaurants/facility/(?{facility}.*)')
+@app.route('/restaurants/facility/{facility}')
+# @app.route('/restaurants/facility/zuppa')
+def get_facility(facility):
+  cursor.execute("SELECT * FROM public.restaurants WHERE facility = %(facility)s", {"facility":facility})
   records = cursor.fetchall()
   return {'results':str(records)}
 

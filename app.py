0c8e9e2d5a9a764a38ec066f4c7f70ea81d2d058
mikequentel <mikequentel@yahoo.com>
2018-02-24 12:51:01 -0500

    Use urllib.unquote to correct escaped GET parameter.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import urllib
+import cgi
+import sys
 import json
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import sys
 from chalice import Chalice
 app = Chalice(app_name='chalice-gis-demo')
 conn_string = "host='localhost' dbname='businesses' user='postgres' password='postgres'"
@@ -14,23 +16,16 @@ def restaurants():
   records = cursor.fetchall()
   return {'results':str(records)}
 
-# @app.route('/restaurants/facility/(?<facility>.*)')
-# @app.route('/restaurants/facility/(?{facility}.*)')
 @app.route('/restaurants/facility/{facility}')
-# @app.route('/restaurants/facility/zuppa')
 def get_facility(facility):
-  cursor.execute("SELECT * FROM public.restaurants WHERE facility = %(facility)s", {"facility":facility})
+  facility = urllib.unquote(facility)
+  sql = "SELECT * FROM public.restaurants WHERE facility = %s"
+  query = cursor.mogrify(sql, (facility,))
+  print query
+  cursor.execute(query)
   records = cursor.fetchall()
   return {'results':str(records)}
 
-@app.route('/hello/{name}')
-def hello_name(name):
-   # '/hello/james' -> {"hello": "james"}
-   return {'hello': name}
-#
-@app.route('/users', methods=['POST'])
-def create_user():
-    # This is the JSON body the user sent in their POST request.
-    user_as_json = app.current_request.json_body
-    # We'll echo the json body back to the user in a 'user' key.
-    return {'user': user_as_json}
+@app.route('/introspect')
+def introspect():
+  return app.current_request.to_dict()

08a844f3d77526e91df5dfba3b869937077e26aa
mikequentel <mikequentel@yahoo.com>
2018-02-26 22:39:21 -0500

    Changed title of map.html

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if [ -z "${NAME}" ]; then
   exit 1
 fi
 virtualenv --python $(which python) ~/.virtualenvs/$NAME
+# source ~/.virtualenvs/chalice-demo/bin/activate
 source ~/.virtualenvs/$NAME/bin/activate
 pip install chalice
 chalice new-project $NAME
